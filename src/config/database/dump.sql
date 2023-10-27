5732dc24f6910df178a1a95b624ad5cf8667dd63
Davi <lobpreal@gmail.com>
2023-10-27 18:22:05 -0300

    criado banco de dados no dump sql

--- src/config/database/dump.sql
+++ src/config/database/dump.sql
@@ -4,13 +4,26 @@ create table usuarios (
   id serial primary key,
   nome text not null,
   email text not null unique,
-  senha text not null
+  senha text not null,
+  saldo integer not null
 );
 
-create table transacoes (
+create table depositos (
   id serial primary key,
-  comentario text,
   valor integer not null,
-  data date,
-  usuario_id integer references usuarios(id)
+  usuario_id integer not null references usuarios(id)
+);
+
+create table saques (
+  id serial primary key,
+  valor integer not null,
+  usuario_id integer not null references usuarios(id)
+);
+
+create table transferencias (
+  id serial primary key,
+  usuario_id_origem integer not null references usuarios(id),
+  numero_conta_destino integer not null references usuarios(id),
+  valor integer not null,
+  senha text
 );
